d4b677b66d3a7b611654b24b02ed859bdc3f1033
Vasiliy Kiryanov <vasiliy.kiryanov@gmail.com>
2025-11-27 10:53:41 -0500

    Version update

--- qcrawl/__init__.py
+++ qcrawl/__init__.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-__version__ = "0.3.2"
+__version__ = "0.3.3"
 
 from qcrawl import cli, core, exporters, middleware, pipelines, settings, signals, storage, utils
 
